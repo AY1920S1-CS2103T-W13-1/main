a744f4acacdbdd4357770c2f40599c74328fd809
CowSaysBaa <mememe479@gmail.com>
2019-10-28 21:11:19 +0800

    Updated FindCommand and FindCommandParser to test a list of predicates. Updated CommandParserTest.java.

--- src/main/java/seedu/module/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/module/logic/parser/FindCommandParser.java
@@ -1,23 +1,32 @@
 package seedu.module.logic.parser;
 
 import static seedu.module.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.module.commons.core.Messages.MESSAGE_INVALID_SEARCH_FIELD;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.function.Predicate;
 
 import seedu.module.logic.commands.FindCommand;
 import seedu.module.logic.parser.exceptions.ParseException;
-import seedu.module.model.module.predicate.NameContainsKeywordsPredicate;
+import seedu.module.model.module.Module;
+import seedu.module.model.module.predicate.ModuleCodeContainsKeywordsPredicate;
 
 /**
- * Parses input arguments and creates a new FindCommand object
+ * Parses input arguments and creates a new FindCommand object.
  */
 public class FindCommandParser implements Parser<FindCommand> {
+
+    private final ArrayList<String> prefixes = new ArrayList<>() {
+        {
+            add("module\\");
+        }
+    };
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns a FindCommand object for execution.
      *
-     * @throws ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform the expected format.
      */
     public FindCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
@@ -27,8 +36,72 @@ public class FindCommandParser implements Parser<FindCommand> {
         }
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
+        ArrayList<ArrayList<String>> listOfLists = parseList(nameKeywords);
+        ArrayList<Predicate<Module>> listOfPredicates = parseListOfLists(listOfLists);
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        return new FindCommand(listOfPredicates);
+    }
+
+    /**
+     * Parses the given {@code ArrayList<String>} of argument and returns a {@code ArrayList<ArrayList<Strings>>}
+     * representing the command prefix and associating keywords.
+     *
+     * @throws ParseException if the user input does not conform the expected format.
+     */
+    ArrayList<ArrayList<String>> parseList(String[] arr) throws ParseException {
+        ArrayList<ArrayList<String>> listOfLists = new ArrayList<>();
+        ArrayList<String> currentList = new ArrayList<>();
+
+        int counter = 1;
+        for (String word : arr) {
+            if (prefixes.contains(word)) {
+                listOfLists.add(currentList);
+                currentList = new ArrayList<>();
+                currentList.add(word);
+            } else {
+                currentList.add(word);
+            }
+            if (counter == arr.length) {
+                listOfLists.add(currentList);
+            }
+            counter++;
+        }
+
+        listOfLists.remove(0);
+
+        if (listOfLists.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        for (ArrayList<String> list : listOfLists) {
+            if (list.size() <= 1) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_SEARCH_FIELD));
+            }
+        }
+
+        return listOfLists;
+    }
+
+    /**
+     * Parses the given {@code ArrayList<ArrayList<Strings>>} of argument and returns a
+     * {@code ArrayList<Predicate<Module>>} representing the predicates for each filter.
+     */
+    ArrayList<Predicate<Module>> parseListOfLists(ArrayList<ArrayList<String>> listOfLists) {
+        assert !listOfLists.isEmpty() : "listOfList is empty.";
+
+        ArrayList<Predicate<Module>> listOfPredicates = new ArrayList<>();
+
+        for (ArrayList<String> list : listOfLists) {
+            switch (list.get(0)) {
+            case "module\\":
+                list.remove(0);
+                listOfPredicates.add(new ModuleCodeContainsKeywordsPredicate(list));
+            }
+        }
+
+        return listOfPredicates;
     }
 
 }
--- src/test/java/seedu/module/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/module/logic/parser/FindCommandParserTest.java
@@ -1,35 +1,154 @@
-// package seedu.module.logic.parser;
+package seedu.module.logic.parser;
 
-// import static seedu.module.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-// import static seedu.module.logic.parser.CommandParserTestUtil.assertParseFailure;
-// import static seedu.module.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static seedu.module.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.module.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.module.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
-// import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.function.Predicate;
 
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-// import seedu.module.logic.commands.FindCommand;
-// import seedu.module.model.module.predicate.NameContainsKeywordsPredicate;
+import seedu.module.logic.commands.FindCommand;
+import seedu.module.logic.parser.exceptions.ParseException;
+import seedu.module.model.module.Module;
+import seedu.module.model.module.predicate.ModuleCodeContainsKeywordsPredicate;
 
-// public class FindCommandParserTest {
+/**
+ * As we are only doing white-box testing, our test cases do not cover path variations
+ * outside of the FindCommandParser code. For example, inputs "1" and "1 abc" take the
+ * same path through the DeleteCommand, and therefore we test only one of them.
+ * The path variation for those two cases occur inside the ParserUtil, and
+ * therefore should be covered by the ParserUtilTest.
+ */
+public class FindCommandParserTest {
+    private static String VALID_STRING = "module\\ cs";
+    private FindCommandParser parser = new FindCommandParser();
+    private ArrayList<String> validModuleArguments = new ArrayList<>() {
+        {
+            add("cs");
+        }
+    };
+    private ArrayList<Predicate<Module>> validPredicateList = new ArrayList<>() {
+        {
+            add(new ModuleCodeContainsKeywordsPredicate(validModuleArguments));
+        }
+    };
 
-//     private FindCommandParser parser = new FindCommandParser();
 
-//     @Test
-//     public void parse_emptyArg_throwsParseException() {
-//         assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-//              FindCommand.MESSAGE_USAGE));
-//     }
+    @Test
+    public void parseList_validArgs_returnsListOfLists() throws ParseException {
+        String[] keywords = {"module\\", "cs"};
+        ArrayList<ArrayList<String>> correctList = new ArrayList<>();
+        ArrayList<String> correctModule = new ArrayList<>() {
+            {
+                add("module\\");
+                add("cs");
+            }
+        };
+        correctList.add(correctModule);
+        assertEquals(correctList, parser.parseList(keywords));
+    }
 
-//     @Test
-//     public void parse_validArgs_returnsFindCommand() {
-//         // no leading and trailing whitespaces
-//         FindCommand expectedFindCommand =
-//                 new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
-//         assertParseSuccess(parser, "Alice Bob", expectedFindCommand);
+    @Test
+    public void parseList_invalidArgsAndValidArgs_returnsListOfLists() throws ParseException {
+        String[] keywords = {"INVALID TEXT", "INVALID TEXT 2", "module\\", "cs"};
+        ArrayList<ArrayList<String>> correctList = new ArrayList<>();
+        ArrayList<String> correctModule = new ArrayList<>() {
+            {
+                add("module\\");
+                add("cs");
+            }
+        };
+        correctList.add(correctModule);
+        assertEquals(correctList, parser.parseList(keywords));
+    }
 
-//         // multiple whitespaces between keywords
-//         assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
-//     }
+    @Test
+    public void parseList_validArgsWithNonsenseFields_returnsListOfLists() throws ParseException {
+        String[] keywords = {"module\\", "mod\\"};
+        ArrayList<ArrayList<String>> correctList = new ArrayList<>();
+        ArrayList<String> correctModule = new ArrayList<>() {
+            {
+                add("module\\");
+                add("mod\\");
+            }
+        };
+        correctList.add(correctModule);
+        assertEquals(correctList, parser.parseList(keywords));
+    }
 
-// }
+    @Test
+    public void parseList_InvalidArgs_throwsParseException() {
+        String[] keywords = {"INVALID INPUT"};
+        assertThrows(ParseException.class, () -> parser.parseList(keywords));
+    }
+
+    @Test
+    public void parseList_emptyFields_throwsParseException() {
+        String[] keywords = {"module\\"};
+        assertThrows(ParseException.class, () -> parser.parseList(keywords));
+    }
+
+    @Test
+    public void parseListOfLists_validArgs_returnsListOfLists() {
+        ArrayList<ArrayList<String>> inputList = new ArrayList<>();
+        ArrayList<String> correctModule = new ArrayList<>() {
+            {
+                add("module\\");
+                add("cs");
+                add("ma");
+            }
+        };
+        inputList.add(correctModule);
+
+        ArrayList<ModuleCodeContainsKeywordsPredicate> correctList = new ArrayList<>();
+        ArrayList<String> moduleArguments = new ArrayList<>() {
+            {
+                add("cs");
+                add("ma");
+            }
+        };
+        ModuleCodeContainsKeywordsPredicate correctModulePredicate = new ModuleCodeContainsKeywordsPredicate(moduleArguments);
+        correctList.add(correctModulePredicate);
+
+        assertEquals(correctList, parser.parseListOfLists(inputList));
+    }
+
+    @Test
+    public void parseListOfLists_validArgsUpperCase_returnsListOfLists() {
+        ArrayList<ArrayList<String>> inputList = new ArrayList<>();
+        ArrayList<String> correctModule = new ArrayList<>() {
+            {
+                add("module\\");
+                add("CS");
+                add("MA");
+            }
+        };
+        inputList.add(correctModule);
+
+        ArrayList<ModuleCodeContainsKeywordsPredicate> correctList = new ArrayList<>();
+        ArrayList<String> moduleArguments = new ArrayList<>() {
+            {
+                add("cs");
+                add("ma");
+            }
+        };
+        ModuleCodeContainsKeywordsPredicate correctModulePredicate = new ModuleCodeContainsKeywordsPredicate(moduleArguments);
+        correctList.add(correctModulePredicate);
+
+        assertEquals(correctList, parser.parseListOfLists(inputList));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        assertParseSuccess(parser, VALID_STRING, new FindCommand(validPredicateList));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+}
